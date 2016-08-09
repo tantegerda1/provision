33c3bf33b55df0c6325d2cfed4630f7226cd3002
Ludwig Rafelsberger <ludwig.rafelsberger@gmx.at>
2016-08-09 09:54:39 +0200

    [BUGFIX] Remove correct files

--- debian/jessie/apache.sh
+++ debian/jessie/apache.sh
@@ -12,8 +12,9 @@
 main() {
 
 	apt-get --quiet -o=Dpkg::Use-Pty=0 --assume-yes --no-install-recommends install apache2
-	rm --force /etc/apache2/sites-available/default
-	rm --force /etc/apache2/sites-enabled/default
+	rm --force /etc/apache2/sites-available/000-default.conf
+	rm --force /etc/apache2/sites-available/default-ssl.conf
+	rm --force /etc/apache2/sites-enabled/000-default.conf
 
 	systemctl restart apache2
 }

024012c31b87a704ba208ffee8a207185ca8f957
Ludwig Rafelsberger <ludwig.rafelsberger@gmx.at>
2016-08-10 08:46:59 +0200

    [TASK] Cleanly log php errors

--- debian/jessie/php.sh
+++ debian/jessie/php.sh
@@ -12,22 +12,33 @@
 main() {
 	apt-get --quiet -o=Dpkg::Use-Pty=0 --assume-yes --no-install-recommends install php5-fpm php5-gd php5-mysqlnd php5-curl php5-mcrypt
 
-
-	if grep --extended-regex --quiet '^;?include=/etc/php5/fpm/\*.conf$' /etc/php5/fpm/php-fpm.conf ; then
-		sed --regexp-extended --in-place 's@^;?include=/etc/php5/fpm/*.conf@include=/etc/php5/fpm/*.conf@' /etc/php5/fpm/php-fpm.conf
+	if grep --extended-regex --quiet '^;?error_log ?=.*$' /etc/php5/fpm/php-fpm.conf ; then
+		sed --regexp-extended --in-place 's@^;?error_log ?=.*@error_log = syslog@' /etc/php5/fpm/php-fpm.conf
 	else
-		echo 'include=/etc/php5/fpm/*.conf' >> /etc/php5/fpm/php-fpm.conf
+		echo 'error_log = syslog' >> /etc/php5/fpm/php-fpm.conf
 	fi
 
-	cat > /etc/php5/fpm/conf.d/30-log.ini << EOF
-error_log = /var/log/php-fpm/error.log
+
+	if grep --extended-regex --quiet '^;?access.log *=.*$' /etc/php5/fpm/pool.d/www.conf ; then
+		sed --regexp-extended --in-place 's@^;?access.log *=.*@access.log = /var/log/php/access.log@' /etc/php5/fpm/pool.d/www.conf
+	else
+		echo 'access.log = /var/log/php/access.log' >> /etc/php5/fpm/pool.d/www.conf
+	fi
+
+
+	mkdir -p /etc/php5/conf-available
+	cat > /etc/php5/conf-available/log.ini << EOF
 log_errors = on
+error_log = /var/log/php/error.log
 EOF
+	ln --force --symbolic ../../conf-available/log.ini /etc/php5/cli/conf.d/30-log.ini
+	ln --force --symbolic ../../conf-available/log.ini /etc/php5/fpm/conf.d/30-log.ini
 
 	cat > /etc/php5/fpm/pool.d/www-changes.conf << EOF
 [www]
 clear_env = no
 EOF
+
 
 	mkdir --parents /etc/systemd/system/php5-fpm.service.d/
 	cat > /etc/systemd/system/php5-fpm.service.d/environment_php5-fpm.conf << EOF
@@ -36,11 +47,11 @@ EnvironmentFile=/etc/environment
 EOF
 	systemctl daemon-reload
 
-	mkdir --parents /var/log/php-fpm/
-	touch /var/log/php-fpm/error.log
-	chown --recursive www-data:adm /var/log/php-fpm
-	chmod 755 /var/log/php-fpm
-	chmod 644 /var/log/php-fpm/error.log
+
+	rm --force /var/log/php5-fpm.log
+	mkdir --parents /var/log/php/
+	chown --recursive www-data:adm /var/log/php
+	chmod 775 /var/log/php
 	systemctl restart php5-fpm
 }
 
